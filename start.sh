d75e4f7dff67ee038af4f76d976106c9332e04fa
shlakoblock-dev <isilantev97@gmail.com>
2026-05-01 04:55:38 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Вставь сюда свой Secret Key из playit.gg
-PLAYIT_SECRET="514e2bd3d78ade26ef14b93a9a658bf79a6800f408d13d9c9eef25eaf0a22bb6"
+PLAYIT_SECRET="hz"
 
 echo "Запускаем playit.gg агент..."
-playit -s "$PLAYIT_SECRET" --tcp 25565 &
+playit -s "514e2bd3d78ade26ef14b93a9a658bf79a6800f408d13d9c9eef25eaf0a22bb6" --tcp 25565 &
 sleep 5
 
 echo "Запускаем Minecraft сервер..."
