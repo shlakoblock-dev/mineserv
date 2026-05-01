8433b504fcf1c397233e5c8de91918109343ee28
shlakoblock-dev <isilantev97@gmail.com>
2026-05-01 04:19:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-# Проверяем токен
 if [ -z "$NGROK_AUTH_TOKEN" ]; then
-  echo "ОШИБКА: не задан NGROK_AUTH_TOKEN в переменных окружения Render!"
+  echo "ОШИБКА: переменная NGROK_AUTH_TOKEN не задана!"
   exit 1
 fi
 
-# Авторизуем и запускаем ngrok
-ngrok config add-authtoken "$NGROK_AUTH_TOKEN"
+echo "Авторизуем ngrok..."
+ngrok authtoken "$NGROK_AUTH_TOKEN"
 echo "Запускаем ngrok..."
 ngrok tcp 25565 --log=stdout &
+sleep 3
 
-# Ждём, чтобы туннель поднялся
-sleep 5
-
-# Запускаем сервер (автоматически берём любой forge-универсал)
 echo "Запускаем Minecraft сервер..."
-exec java -Xmx1G -jar /app/forge-*.jar nogui
+exec java -Xmx1G -jar $(ls /app/forge-*.jar | head -n 1) nogui
