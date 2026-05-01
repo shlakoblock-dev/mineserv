fe1d19195f4842c945c9a5f15c4a4194e20ab0e8
shlakoblock-dev <isilantev97@gmail.com>
2026-05-01 13:40:42 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-PLAYIT_SECRET="514e2bd3d78ade26ef14b93a9a658bf79a6800f408d13d9c9eef25eaf0a22bb6"
-
-echo "Запускаем playit.gg..."
-playit --secret "$PLAYIT_SECRET" &
+echo "Запускаем pinggy туннель..."
+# Пробрасываем порт 25565 наружу одной командой
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -R 0:localhost:25565 a.pinggy.io &
 sleep 5
 
 echo "Запускаем Paper сервер 1.20.1..."
