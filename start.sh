c7295f35336550d61ce5f9ae424ce241a26dd47b
shlakoblock-dev <isilantev97@gmail.com>
2026-05-01 04:08:06 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
-
-# Проверяем, что токен задан
 if [ -z "$NGROK_AUTH_TOKEN" ]; then
   echo "Ошибка: не найден NGROK_AUTH_TOKEN в переменных окружения!"
   exit 1
 fi
-
-# Авторизуем ngrok
-ngrok config add-authtoken "3D6BfjFOACUPaerNo6YA4Jp6IGi_6JbDA5dLLngu8dEDtXemW"
-
-# Запускаем ngrok в фоновом режиме (tcp-туннель на порт 25565)
+ngrok config add-authtoken "$NGROK_AUTH_TOKEN"
 ngrok tcp 25565 --log=stdout &
-
-# Ждём секунду, чтобы ngrok успел подняться
 sleep 2
-
-# Запускаем Forge сервер как основной процесс
-exec java -Xmx1G -jar forge-1.20.1-47.2.17-universal.jar nogui
+exec java -Xmx1G -jar $(ls /app/forge-*.jar | head -n 1) nogui
